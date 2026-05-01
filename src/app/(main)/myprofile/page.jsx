83902093eba165680aa2f4ede4da8b94f4bf2e87
Kallol Dey <kalloldey067@gmail.com>
2026-05-01 18:21:12 +0600

    update profile done

--- src/app/(main)/myprofile/page.jsx
+++ src/app/(main)/myprofile/page.jsx
@@ -1,15 +1,40 @@
-import Link from "next/link";
+import { auth } from "@/lib/auth";
+import { headers } from "next/headers";
+import Image from "next/image";
 
+const MyProfilePage = async () => {
 
-const MyProfilePage = () => {
+    const session = await auth.api.getSession({
+        headers: await headers(),
+    });
+
     return (
         <div className="mx-5 md:mx-30 lg:mx-65 mt-10 shadow-sm h-[50vh] pt-20">
             <div className="text-center space-y-5">
-                <h2>Name: Hello</h2>
-                <p>Email: kalloldey067@gmail.com</p>
-                <Link href={'/updateprofile'}>
-                <button className="btn btn-primary">Update</button>
-                </Link>
+
+
+                {
+                    session?.user?.image && (
+                        <Image
+                            src={session.user.image}
+                            alt="profile"
+                            width={80}
+                            height={80}
+                            className="rounded-full mx-auto"
+                        />
+                    )
+                }
+
+
+                <h2>Name: {session?.user?.name}</h2>
+                <p>Email: {session?.user?.email}</p>
+
+
+
+                <a href="/updateprofile">
+                    <button className="btn btn-primary">Update</button>
+                </a>
+
             </div>
         </div>
     );
--- src/app/(main)/updateprofile/page.jsx
+++ src/app/(main)/updateprofile/page.jsx
@@ -1,23 +1,63 @@
-import Link from "next/link";
+'use client'
+import { authClient } from "@/lib/auth-client";
+import { useState } from "react";
+import { redirect } from "next/navigation";
+import { toast } from "react-toastify";
 
 const UpdateFormPage = () => {
+  const [name, setName] = useState("");
+  const [image, setImage] = useState("");
+
+  const handleUpdate = async () => {
+    const { data, error } = await authClient.updateUser({
+      name: name,
+      image: image,
+    });
+
+    if (error) {
+      toast.error("Update failed user logged out");
+      redirect("/login")
+    }
+
+    if (data) {
+      toast.success("Profile updated successfully");
+      redirect("/myprofile");
+    }
+
+
+
+  };
+
   return (
     <div className="mx-5 md:mx-30 lg:mx-65 shadow-sm h-[50vh] mt-10 pt-20 text-center">
 
       <div className="flex flex-col gap-10 items-center">
 
-        
         <div className="w-1/3 flex flex-col gap-6">
-          <input className="border border-black p-2" type="text" placeholder="Type your name"/>
+
 
           <input
-            className="border border-black p-2" type="text" placeholder="Type your image url"/>
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+            className="border border-black p-2"
+            type="text"
+            placeholder="Type your name"
+          />
 
-          
+
+          <input
+            value={image}
+            onChange={(e) => setImage(e.target.value)}
+            className="border border-black p-2"
+            type="text"
+            placeholder="Type your image url"
+          />
+
+
           <div className="flex justify-end">
-            <Link href="/myprofile">
-              <button className="btn btn-primary">Done</button>
-            </Link>
+            <button onClick={handleUpdate} className="btn btn-primary">
+              Done
+            </button>
           </div>
 
         </div>
