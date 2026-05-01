5374f8ea589ac80a87243883568bef48e595a19e
Kallol Dey <kalloldey067@gmail.com>
2026-05-01 22:08:36 +0600

    loading page designed

---
+++ src/app/(main)/loading.jsx
@@ -0,0 +1,11 @@
+
+
+const MainLoading = () => {
+    return (
+        <div className="flex justify-center items-center mt-50 mb-50">
+            <span className="loading loading-dots loading-xl "></span>
+        </div>
+    );
+};
+
+export default MainLoading;
